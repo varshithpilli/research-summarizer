45ef9387c74a299d463a42b1b0750c25abc05af2
varshith pilli <varshith6946@gmail.com>
2025-07-26 15:13:52 +0530

    backend:01

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://re-summ.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
