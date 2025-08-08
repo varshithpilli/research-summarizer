14ae300b90f2d2a157ba9609f3c40b1479494da7
varshith pilli <varshith6946@gmail.com>
2025-08-08 16:48:13 +0530

    actions03

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bl03.varzone.in/"],
+    allow_origins=["https://bl03.varzone.in"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
